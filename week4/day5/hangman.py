46e183e5076752aac7d241f300b77e7cf3a3c666
SapDov <Sap@fff.com>
2021-08-08 01:01:49 +0300

    adding new files

--- week4/day5/hangman.py
+++ week4/day5/hangman.py
@@ -1,66 +1,85 @@
-import random
-import re
-
-def print_gallows():
-    gallows = ['0', '/', '|', '\\', '/', '\\']
-    fn = gallows[:counter] + ["" for num in range(0,6-counter)]
-    print(fn)
-    print(*fn)
-    print('''
-    ____
+hangman = {
+0:
+"""      +---+
+       	   |
+      	   |
+      	   |
+       	  ===""",
+1:
+"""   +---+
+    O   |
+        |
+        |
+       ===""",
+2:
+"""   +---+
+    O   |
     |   |
-    |   {}
-    |  {}{}{} 
-    |  {} {}
-    '''.format(*gallows))
-
-def is_game_over():
+        |
+       ===""",
+3:
+"""   +---+
+    O   |
+   /|   |
+        |
+       ===""",
+4:
+"""   +---+
+    O   |
+   /|\\ |
+        |
+       ===""",
+5:
+"""   +---+
+    O   |
+   /|\\ |
+   /    |
+       ===""",
+6:
+"""   +---+
+    O   |
+   /|\\ |
+   / \\ |
+       ==="""
+}
+import random, re
+wordslist = ['correction', 'childish', 'beach', 'python', 'assertive', 'interference', 'complete', 'share', 'credit card', 'rush', 'south']
+guessed_letter = []
+lives = 0
+def display_hangman(lives):
+	print(hangman[lives])
+def is_game_over(word):
 	if "".join(hidden_word) == word:
 		print("You won!!")
 		return True
-	elif counter > 5:
+	elif lives > 7:
 		print("Yoy killed Kenny")
 		return True
 	else:
 		return False
-wordslist = ['correction']
 word = random.choice(wordslist)
 hidden_word = list(re.sub(r'[a-zA-Z]', '*', word))
-counter=0
-guessed_letter = []
-while True:
-	print_gallows()
-	if is_game_over():
+while True:	
+	print("".join(hidden_word))
+	display_hangman(lives)
+	if is_game_over(word):
 		break
-		print("these are the letters you\'ve already guessed: ", *guessed_letter)
+		print("these are the letters you\"ve already guessed: ", *guessed_letter)
 		print("".join(hidden_word))
-
 	while True:
-		user_letter = input("Guess a letter you haven\'t guessed yet: ")
+		user_letter = input("Guess a letter you haven\"t guessed yet: ")
 		if len(user_letter) > 1:
 			print ("too many letters, try again")
 		elif not user_letter.isalpha():
-			print("not a letter, you\'re better than that")
+			print("not a letter, you\"re better than that")
 		elif user_letter in guessed_letter:
-			print("you\'ve already guessed that")
+			print("you\"ve already guessed that")
 		else:
 			guessed_letter.append(user_letter)
 			break
 	if user_letter in word:
-		for index, letter in enumerate (word):
+		for index, letter in enumerate(word):
 			if user_letter == letter:
-				hidden_word[index] == user_letter
+				hidden_word[index] = user_letter
 	else:
-		counter += 1
-
-
-
-
-
-
-
-
-
-
-
-
+		lives = lives + 1
